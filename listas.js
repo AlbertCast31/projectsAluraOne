ec41845aeb46b9ad30dfe2d0f12d1e051d65d987
AlbertCast31 <beto31cast@gmail.com>
2025-02-14 17:35:47 -0600

    Agregamos elementos a la lista y creamos una función para mostrar todos los elementos de esa lista

--- listas.js
+++ listas.js
@@ -6,3 +6,19 @@ let listaGenerica=[];
 
 let lenguajesDeProgramacion=['JavaScript', 'C', 'C++', 'Kotlin','Python'];
 
+// 3 Agregamos los siguientes elementos 'Java','Ruby' y 'GoLang' a la lista con push
+
+lenguajesDeProgramacion.push('Java','Ruby','GoLang');
+
+// 4 Creamos una funcion que muestre en la consola todos los elementos de la lista "lenguajesDeProgramacion"
+
+function mostrarLista(){
+
+    for (let i=0; i<lenguajesDeProgramacion.length; i++){
+        console.log(lenguajesDeProgramacion[i]);
+    }
+}
+
+
+
+
