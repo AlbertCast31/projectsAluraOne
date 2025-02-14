be3b669fe8f2a0e59133448a1cee189e186f79d0
AlbertCast31 <beto31cast@gmail.com>
2025-02-14 17:04:52 -0600

    creamos una lista de lenguajes

--- listas.js
+++ listas.js
@@ -1,3 +1,8 @@
 //1 Generar lista generica
 
 let listaGenerica=[];
+
+//2 Crea una lista de lenguajes de programación llamada "lenguagesDeProgramacion" 
+
+let lenguajesDeProgramacion=['JavaScript', 'C', 'C++', 'Kotlin','Python'];
+
