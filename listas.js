9917c82a8b1754e17f5e15e5c9994dbc87898bf0
AlbertCast31 <beto31cast@gmail.com>
2025-02-14 20:15:02 -0600

    Creamos una función para mostrar lista de manera inversa, se crean una lista de numeros para calcular su promedio.

--- listas.js
+++ listas.js
@@ -19,6 +19,33 @@ function mostrarLista(){
     }
 }
 
+// 5 Creamos una función para mostrar la lista en orden inverso
+
+function mostrarListaInversa(){
+
+    for (let i= lenguajesDeProgramacion.length -1; i>=0; i-- ){
+        console.log(lenguajesDeProgramacion[i]);
+    }
+}
+
+// Creamos una lista de números para pruebas de cálculo
+
+let listaNumerosA=[15,30,45,53,60];
+let listaNumerosB=[5,18,21,33,48];
+
+// 6 Creamos una funcion para calcular el promedio de una lista de números
+
+function calcularPromedio(){
+
+    let suma=0;
+    for (let i=0; i<listaNumerosA.length; i++){
+        suma += listaNumerosA[i];
+    }
+    let promedio= suma / listaNumerosA.length;
+    console.log("Promedio:", promedio);
+    return promedio;
+}
+
 
 
 
